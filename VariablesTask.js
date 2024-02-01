6f222265c5d251b5badec60195a7b52d1e377586
MadhuMohanS <madhumohan.xworkz@gmail.com>
2024-02-01 23:27:53 +0530

    task

---
+++ VariablesTask.js
@@ -0,0 +1,125 @@
+// Properties of a door
+let doorColor = 'brown';
+var isLocked = false;
+const hasWindow = true;
+let numberOfPanels = 4;
+var isOpen = false;
+const material = 'wood';
+let width = 80; // in centimeters
+var height = 200; // in centimeters
+const hasKnob = true;
+let isAutomatic = false;
+
+// Printing the door properties
+console.log('Door Color:', doorColor);
+console.log('Is Locked:', isLocked);
+console.log('Has Window:', hasWindow);
+console.log('Number of Panels:', numberOfPanels);
+console.log('Is Open:', isOpen);
+console.log('Material:', material);
+console.log('Width:', width, 'cm');
+console.log('Height:', height, 'cm');
+console.log('Has Knob:', hasKnob);
+console.log('Is Automatic:', isAutomatic);
+
+// Locker properties as variables
+let lockerId = 101;
+var lockerSize = 'large';
+const lockerMaterial = 'stainless steel';
+let lockerColor = 'silver';
+var isLockerOpen = false;
+const lockerContents = ['documents', 'electronics'];
+let securityCode = '9876';
+var hasBiometricScanner = true;
+const lockerOwner = 'Alice Johnson';
+let isLockerOccupied = true;
+
+// Print statements
+console.log('Locker ID:', lockerId);
+console.log('Locker Size:', lockerSize);
+console.log('Locker Material:', lockerMaterial);
+console.log('Locker Color:', lockerColor);
+console.log('Is Locker Open?', isLockerOpen ? 'Yes' : 'No');
+console.log('Locker Contents:', lockerContents.join(', '));
+console.log('Security Code:', securityCode);
+console.log('Has Biometric Scanner?', hasBiometricScanner ? 'Yes' : 'No');
+console.log('Owner:', lockerOwner);
+console.log('Is Locker Occupied?', isLockerOccupied ? 'Yes' : 'No');
+
+// Mouse properties as variables
+let mouseBrand = 'Logitech';
+let mouseModel = 'MX Master 3';
+let mouseColor = 'Graphite';
+let isMouseWireless = true;
+let numberOfButtons = 7;
+let isBluetoothEnabled = true;
+let mouseDPI = 4000;
+let hasScrollWheel = true;
+let isMouseRechargeable = true;
+let isMouseConnected = false;
+
+// Print statements
+console.log('Mouse Brand:', mouseBrand);
+console.log('Mouse Model:', mouseModel);
+console.log('Mouse Color:', mouseColor);
+console.log('Is Mouse Wireless?', isMouseWireless ? 'Yes' : 'No');
+console.log('Number of Buttons:', numberOfButtons);
+console.log('Is Bluetooth Enabled?', isBluetoothEnabled ? 'Yes' : 'No');
+console.log('DPI (Dots Per Inch):', mouseDPI);
+console.log('Has Scroll Wheel?', hasScrollWheel ? 'Yes' : 'No');
+console.log('Is Rechargeable?', isMouseRechargeable ? 'Yes' : 'No');
+console.log('Is Mouse Connected?', isMouseConnected ? 'Yes' : 'No');
+
+// Plug properties
+let plugType = 'Type-C';
+var voltage = 220;
+const isSmartPlug = true;
+let hasUSBPorts = 2;
+var plugColor = 'White';
+const isWiFiEnabled = true;
+let amperage = 10;
+var isChildSafe = true;
+const isEnergyEfficient = true;
+let brand = 'SmartHome';
+
+// Print statements
+console.log('Plug Type:', plugType);
+console.log('Voltage (V):', voltage);
+console.log('Is Smart Plug?', isSmartPlug ? 'Yes' : 'No');
+console.log('Number of USB Ports:', hasUSBPorts);
+console.log('Plug Color:', plugColor);
+console.log('Is WiFi Enabled?', isWiFiEnabled ? 'Yes' : 'No');
+console.log('Amperage:', amperage);
+console.log('Is Child Safe?', isChildSafe ? 'Yes' : 'No');
+console.log('Is Energy Efficient?', isEnergyEfficient ? 'Yes' : 'No');
+console.log('Brand:', brand);
+
+// Table properties
+let tableName = 'Dining Table';
+let material1 = 'Wood';
+let color = 'Brown';
+let shape = 'Rectangle';
+let numberOfSeats = 6;
+let isExtendable = true;
+let hasDrawers = false;
+let dimensions = { length: 180, width: 90, height: 75 };
+let isFoldable = false;
+
+
+// Print statements
+console.log('Table Name:', tableName);
+console.log('Material:', material1);
+console.log('Color:', color);
+console.log('Shape:', shape);
+console.log('Number of Seats:', numberOfSeats);
+console.log('Is Extendable?', isExtendable ? 'Yes' : 'No');
+console.log('Has Drawers?', hasDrawers ? 'Yes' : 'No');
+console.log('Dimensions (L x W x H):', dimensions.length + ' x ' + dimensions.width + ' x ' + dimensions.height + ' cm');
+console.log('Is Foldable?', isFoldable ? 'Yes' : 'No');
+console.log('Brand:', brand);
+
+
+
+
+
+
